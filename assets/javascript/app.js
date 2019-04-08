0c189b1baf9f34e8a9612116fcbce7f238a5cd1d
katjadulmaine <katja.dulmaine@gmail.com>
2019-04-08 08:41:15 -0400

    st

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,51 @@
 $(document).ready(function () {
+    var timer = setInterval(countdown, 1000);
     var seconds = 60;
-    var timer = 0;
     var audioElement;
     var score = 0;
-    var questAnswer = { name: ("(.value)") };
+    question
+    var questAnswer = { name:".value"};
     //the list Q1: "wrong" Q2: "correct"
     //var display; is this need with 
-    $(".start").click(function (start) {
-        $(".display1").style.display = "none";
-        $(".display2").style.display = "initial";
-    })
+    $(".start").on("click", function () {
+        $(".display1").hide();
+        $(".display2").show();
+        
+    });
+
+    $(".submit").on("click", function(){
+        clearInterval(timer);
+        $(".display2").hide(); 
+        $(".display3").show();
+        checked();
+    });
     //checked() inside submit button & when time runs out
     function countdown() {
-        timer = setInterval(timerFun, 1000);
-        function timerFun() {
+     
             seconds--;
-            $("#timer").html("<h2>" + seconds + "</h2>");
+            $("#timer").html("<h2>" + seconds +"</h2>");
          if (seconds === 0) {
-            $("#timer").append("<h2>You have run out of time!</h2>");
+            $("#timer").append("<h2>You have run out of time!</h2>");        
             clearInterval(timer);
             audioElement = document.createElement("audio");
             audioElement.setAttribute("src", "assets/audio/championCrop.mp3");
             audioElement.play();
-            setTimeout(8000, $(".display2").style.display = "none", $(".display3").style.display = "initial");
-            //wait 8000 then change displays
-        } else {
-            $(".submit").click($(".display2").style.display = "none", $(".display3").style.display = "initial");
-        }
+            $(".display2").hide(); 
+            $(".display3").show();
+            checked();
+           } 
+    
     }
-    }
-    console.log(countdown);
-    countdown();
+
+    //countdown();
     function checked() {
         $('input.name():checked' + 'input.value():checked').click(questAnswer ={ name : ("value")});
         if (value === "correct") {
             $document("#results").html(questAnswer);
-            score++
-            $("#results").html("Your score is" + score / questAnswer.length + "%");
-            $(".display2").style.display = "none";
-            $(".display3").style.display = "initial";
+            score++;
+            $("#results").html("Your score is" + score / questAnswer.length + "%");                                //not questAnswer but total questions
+            $(".display2").hide();
+            $(".display3").show();
             console.log(score);
         }
     };
