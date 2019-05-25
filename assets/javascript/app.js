d46261019b0cab5a4783e1492a70bb1cd69b219d
katjadulmaine <katja.dulmaine@gmail.com>
2019-05-25 12:37:03 -0400

    improved buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
+var score = 0;
 $(document).ready(function () {
     var timer = setInterval(countdown, 1000);
     var seconds = 60;
-    var score = 0;
-    
+    var audioElement;  
+    var questAnswer = { name: ".value" };
     $(".start").on("click", function () {
         $(".display1").hide();
         $(".display2").show();
-        countdown()
     });
 
     $(".submit").on("click", function () {
@@ -15,31 +15,36 @@ $(document).ready(function () {
         $(".display3").show();
         handleClick();
     });
-
+    //checked() inside submit button & when time runs out
     function countdown() {
         seconds--;
         $("#timer").html("<h2>" + seconds + "</h2>");
         if (seconds === 0) {
             $("#timer").append("<h2>You have run out of time!</h2>");
             clearInterval(timer);
+            audioElement = $("audio");
+            audioElement.setAttribute("src", "assets/audio/championCrop.mp3");
+            audioElement.play();
             $(".display2").hide();
             $(".display3").show();
             handleClick();
         };
     };
 
-    function handleClick() {
+});
+
+
+function handleClick() {
+    console.log("handling click")
         for (let index = 1; index < 6; index++) {
-            let answer = $("input[name='Q" + index + "']:checked").val();
-            if (answer === "correct") {
+            let answer =$("input[name='Q"+index+"']:checked").val();
+            if (answer === "correct"){
                 score++
             }
         }
-
-        console.log("correct answers: " + score)
-        console.log("wrong answers" + (5 - score))
-    };
-    $(".restart").on("click", function(){
-        location.reload;
-    });
-});
+        $('.correct').html("<div class id='stylethis'>correct answers: "+score+"</div>")
+        $('.wrong').html("wrong answers: "+ (5-score))
+};
+$(".restart").on("click",function(){
+    location.reload();
+})
